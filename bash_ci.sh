87b5ca25155a792b4ab5722154a192cda3588c22
Tahsan <TahsanR56@gmail.com>
2026-04-16 23:43:36 +0100

    bash ci script to auto test

---
+++ bash_ci.sh
@@ -0,0 +1,17 @@
+
+# activate venv
+source venv/Scripts/activate
+
+pytest --headless test.py -v
+
+# capture the exit code from pytest
+EXIT_CODE=$?
+
+# return 0 if passed, 1 if failed
+if [ $EXIT_CODE -eq 0 ]; then
+    echo "All tests passed!"
+    exit 0
+else
+    echo "Tests failed!"
+    exit 1
+fi
